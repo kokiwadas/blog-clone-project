14c56f77c95fdb78cf3e0982036adf949d0a61c9
kokiwadas <koki.wadas@gmail.com>
2019-07-11 14:02:06 +0200

    'Added view for creating comments to app's URLs'

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -7,7 +7,8 @@ urlpatterns = [
     path('about/', views.AboutView.as_view(), name='about'),
     re_path('post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_new'),
-    path('post/(?P<pk>\d+)$/edit/', views.PostUpdateView.as_view(), name='post_edit'),
-    path('post/(?P<pk>\d+)$/remove/', views.PostDeleteView.as_view(), name='post_remove'),
+    re_path('post/(?P<pk>\d+)$/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
+    re_path('post/(?P<pk>\d+)$/remove/$', views.PostDeleteView.as_view(), name='post_remove'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    re_path('post/(?P<pk>\d+)$/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
 ]
