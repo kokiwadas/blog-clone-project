268e12688b6b94f0e452837d231278669ceafbc7
kokiwadas <koki.wadas@gmail.com>
2019-07-11 12:36:42 +0200

    'Added view for post details in app's URLs'

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, re_path
 from blog import views
 
 
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
-
+    re_path('post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
 ]
