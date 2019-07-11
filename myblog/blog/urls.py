74a538b4176c1168e28dd83c07da04a2fe3f21af
kokiwadas <koki.wadas@gmail.com>
2019-07-11 14:08:02 +0200

    'Added view for approving comments to app's URLs'

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -5,10 +5,11 @@ from blog import views
 urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
-    re_path('post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
+    re_path(r'^post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
     path('post/new/', views.PostCreateView.as_view(), name='post_new'),
-    re_path('post/(?P<pk>\d+)$/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
-    re_path('post/(?P<pk>\d+)$/remove/$', views.PostDeleteView.as_view(), name='post_remove'),
+    re_path(r'^post/(?P<pk>\d+)$/edit/$', views.PostUpdateView.as_view(), name='post_edit'),
+    re_path(r'^post/(?P<pk>\d+)$/remove/$', views.PostDeleteView.as_view(), name='post_remove'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
-    re_path('post/(?P<pk>\d+)$/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    re_path(r'^post/(?P<pk>\d+)$/comment/$', views.add_comment_to_post, name='add_comment_to_post'),
+    re_path(r'^comment/(?P<pk>\d+)$/approve/$', views.comment_approve, name='comment_approve'),
 ]
