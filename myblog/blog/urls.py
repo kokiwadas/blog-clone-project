31a0da1c4c967abd3c7233b3673add7e135adb13
kokiwadas <koki.wadas@gmail.com>
2019-07-11 12:47:31 +0200

    'Added view for creating a post in app's URLs'

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.PostListView.as_view(), name='post_list'),
     path('about/', views.AboutView.as_view(), name='about'),
     re_path('post/(?P<pk>\d+)$', views.PostDetailView.as_view(), name='post_detail'),
+    path('post/new/', views.CreatePostView.as_view(), name='post_new'),
 ]
