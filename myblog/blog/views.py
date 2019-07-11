d6fa0c6ec80c25af02d4ecdd35daf5026ad6c430
kokiwadas <koki.wadas@gmail.com>
2019-07-11 12:14:10 +0200

    'Added PostListView to display the list of posts. Function get_queryset orders post by published date in descending order'

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from blog.models import Post, Comment
+from django.views.generic import TemplateView, ListView
+from django.utils import timezone
 # Create your views here.
 
 
 class AboutView(TemplateView):
     template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now().order_by('-published_date'))
+
